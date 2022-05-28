0be367a2aade0d88718ed77595e45bbce1beb5c7
veekshith-gandhi <veekshithgandhi@gmail.com>
2022-05-28 13:27:31 +0530

    schema

--- assignment1/models/category.model.js
+++ assignment1/models/category.model.js
@@ -3,5 +3,7 @@
 const mongoos = require("mongoose");
 
 const CategorySchema = new mongoos.Schema({
+  restaurantId: { type: String },
+  dishId: { type: String },
   category: { type: String },
 });
